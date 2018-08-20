a4451890859a5178217c5b2091337d20dcd3ed99
wangzhichao <wangzhichao@adups.com>
2018-08-20 16:45:28 +0800

    fine-tuning

--- library/src/main/java/com/wzc/labelview/LabelView.java
+++ library/src/main/java/com/wzc/labelview/LabelView.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -20,9 +21,10 @@ public class LabelView extends View {
     float mTextHeight;
     float mTextWidth;
     int mTextStyle;
-    float mTopMargin;
-    float mBottomPadding;
-    float mTopPadding;
+    float mTextBottomPadding;
+    float mTextTopPadding;
+    float mTextLeftPadding;
+    float mTextRightPadding;
     Paint mTrapezoidPaint;
     int mBackGroundColor;
     float mDegrees;
@@ -33,6 +35,7 @@ public class LabelView extends View {
     public static final int DEGREES_LEFT = -45;
     public static final int DEGREES_RIGHT = 45;
     private Path mPath;
+    private Paint.FontMetricsInt mFontMetricsInt;
 
 
     public LabelView(Context context) {
@@ -48,23 +51,16 @@ public class LabelView extends View {
 
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.LabelView);
 
-        mTopMargin = ta.getDimension(R.styleable.LabelView_label_top_margin, dp2px(7));
-
-        mTopPadding = ta.getDimension(R.styleable.LabelView_label_top_padding, dp2px(3));
-
-        mBottomPadding = ta.getDimension(R.styleable.LabelView_label_bottom_padding, dp2px(3));
-
-        mBackGroundColor = ta.getColor(R.styleable.LabelView_label_background_color, Color.parseColor("#66000000"));
-
-        mTextColor = ta.getColor(R.styleable.LabelView_label_text_color, Color.WHITE);
-
-        mTextSize = ta.getDimension(R.styleable.LabelView_label_text_size, sp2px(11));
-
+        mTextLeftPadding = ta.getDimension(R.styleable.LabelView_label_text_left_padding, getResources().getDimension(R.dimen.default_padding));
+        mTextTopPadding = ta.getDimension(R.styleable.LabelView_label_text_top_padding, getResources().getDimension(R.dimen.default_padding));
+        mTextRightPadding = ta.getDimension(R.styleable.LabelView_label_text_right_padding, getResources().getDimension(R.dimen.default_padding));
+        mTextBottomPadding = ta.getDimension(R.styleable.LabelView_label_text_bottom_padding, getResources().getDimension(R.dimen.default_padding));
+        mBackGroundColor = ta.getColor(R.styleable.LabelView_label_background_color, getResources().getColor(R.color.default_background_color));
+        mTextColor = ta.getColor(R.styleable.LabelView_label_text_color, getResources().getColor(R.color.default_text_color));
+        mTextSize = ta.getDimension(R.styleable.LabelView_label_text_size, getResources().getDimension(R.dimen.default_text_size));
         mText = ta.getString(R.styleable.LabelView_label_text);
-
         mTextStyle = ta.getInt(R.styleable.LabelView_label_text_style, TEXT_STYLE_BOLD);
-
-        mDegrees = ta.getInt(R.styleable.LabelView_label_direction, 45);
+        mDegrees = ta.getInt(R.styleable.LabelView_label_direction, DEGREES_LEFT);
 
         ta.recycle();
 
@@ -91,6 +87,8 @@ public class LabelView extends View {
     private static final int TEXT_STYLE_NORMAL = 0;
     private static final int TEXT_STYLE_ITALIC = 1;
     private static final int TEXT_STYLE_BOLD = 2;
+    private RectF mRect = new RectF();
+
     private void initTextPaint() {
 
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -119,16 +117,22 @@ public class LabelView extends View {
         mTextPaint.getTextBounds(mText, 0, mText.length(), rectText);
         mTextWidth = rectText.width();
         mTextHeight = rectText.height();
+        mFontMetricsInt = mTextPaint.getFontMetricsInt();
+        mTextHeight = mFontMetricsInt.bottom - mFontMetricsInt.top + mTextBottomPadding + mTextTopPadding;
+
+
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        height = (int) (mTopMargin + mTopPadding + mBottomPadding + mTextHeight);
-        width = 2 * height;
 
+        width = (int) (mTextWidth + mTextLeftPadding + mTextRightPadding + mTextHeight * 2);
+
+        height = width / 2;
 
         int realHeight = (int) (height * Math.sqrt(2));
+
         setMeasuredDimension(width, realHeight);
     }
 
@@ -136,26 +140,29 @@ public class LabelView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-
         canvas.translate(0, (float) ((height * Math.sqrt(2)) - height));
         if (mDegrees == DEGREES_LEFT) {
             canvas.rotate(mDegrees, 0, height);
         } else if (mDegrees == DEGREES_RIGHT) {
             canvas.rotate(mDegrees, width, height);
         }
-
+        mRect.left = mTextHeight;
+        mRect.top = height - mTextHeight;
+        mRect.right = mRect.left + mTextWidth + mTextLeftPadding + mTextRightPadding;
+        mRect.bottom = height;
 
         mPath.moveTo(0, height);
-        mPath.lineTo(width / 2 - mTopMargin, mTopMargin);
-        mPath.lineTo(width / 2 + mTopMargin, mTopMargin);
+        mPath.lineTo(mRect.left, mRect.top);
+        mPath.lineTo(mRect.right, mRect.top);
         mPath.lineTo(width, height);
         mPath.close();
-
         canvas.drawPath(mPath, mTrapezoidPaint);
 
-        canvas.drawText(mText, width / 2, mTopMargin + mTextHeight + mTopPadding, mTextPaint);
-    }
+        float baseline = mRect.centerY() - (mFontMetricsInt.top + mFontMetricsInt.bottom) / 2;
+        canvas.drawText(mText, mRect.centerX(), baseline, mTextPaint);
 
+
+    }
 
 
     public int dp2px(float dpValue) {
